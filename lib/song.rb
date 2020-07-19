d92505f7018c2f477641bc23b661718ab3e0416d
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-07-19 20:10:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,18 @@
 class Song
-  
+
   attr_accessor :name, :artist
-  
+
   @@all =[]
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.new_by_filename(file)
     array = file.split (' - ')
     song = Song.new(array[1])
