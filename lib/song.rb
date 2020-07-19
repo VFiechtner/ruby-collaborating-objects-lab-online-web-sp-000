c1e8ba8848a57829b046329cb736b60c975a2ece
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-07-19 20:10:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,34 @@
 class Song
-
+  
   attr_accessor :name, :artist
-
+  
+  @@all =[]
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
-
-
+  
+  def self.all
+    @@all
+  end
+  
+  def self.new_by_filename(file)
+    array = file.split (' - ')
+    song = Song.new(array[1])
+    song.artist = Artist.find_or_create_by_name(array[0])
+    song
+  end
+  
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
-  def self.new_by_filename(file_name)
-    song = file_name.split(" - ")[1]
-    artist = file_name.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    Artist.all.each do |artist|
+      if artist.name == name
+        self.artist = artist
+      end
+    end
+    if self.artist == nil
+        self.artist =   Artist.new(name)
+    end
   end
 
 end
